26e9d9bbf7fa7e077294e7a95446fc5c00f3a684
SquidCaptain <george.y.yeh@gmail.com>
2022-01-09 08:10:22 -0500

    added app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "main page"
+
+@app.route("/input")
+def input():
+    return "input"
+
+@app.route("/edit")
+def edit():
+    return "edit"
+
+@app.route("/delete")
+def delete():
+    return "delete"
+
+@app.route("/view")
+def view():
+    return "view"
