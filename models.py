a29440fd80593a21405f77b402cfb7d54fac9b53
SquidCaptain <35849316+SquidCaptain@users.noreply.github.com>
2022-01-11 01:11:36 -0500

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(140))
-    inventory = db.Column(db.Integer)
-    price = db.Column(db.Integer)
-    slug = db.Column(db.String(140), unique=True)
-    
