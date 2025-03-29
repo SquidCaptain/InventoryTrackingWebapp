ffc2dd3960a87935afe76d504615069724ce7279
George <george.y.yeh@gmail.com>
2025-03-29 00:46:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from config import Config
 ## --Flask App--
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SQLALCHEMY_POOL_SIZE'] = 10
+app.config['SQLALCHEMY_MAX_OVERFLOW'] = 20
 
 ## --Database--
 db = SQLAlchemy(app)
