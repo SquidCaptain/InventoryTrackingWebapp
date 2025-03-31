68a5771ba7fd1f78a69f526747faf2ccc26b9b8e
George <george.y.yeh@gmail.com>
2025-03-31 00:25:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 ## --Database--
-db = SQLAlchemy(app)
-# try:
-#     with app.app_context():
-#         db.create_all()
-#         print("Database initialized successfully.")
-# except Exception as e:
-#     print(f"Database connection failed: {e}")
+try:
+    db = SQLAlchemy(app)
+    with app.app_context():
+        db.create_all()
+        print("Database initialized successfully.")
+except Exception as e:
+    print(f"Database connection failed: {e}")
